136acc78fde790ee4e798928fb882797cfaad284
Lu(c)ky/Coder <lukasbriza@seznam.cz>
2022-08-27 21:39:40 +0200

    init

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+        transcludeMarkdown: true,
+      },
+    },
     "@storybook/addon-essentials",
-    "@storybook/preset-scss"
+    "@storybook/preset-scss",
   ],
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  core: {
+    builder: "webpack5",
+  },
+};
