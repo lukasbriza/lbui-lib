b2d95963f33a3d9f6b678ec83cff522c268a01a5
Lu(c)ky/Coder <lukasbriza@seznam.cz>
2022-09-17 16:03:51 +0200

    rollup configuration and export update

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,10 @@ import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 import svgr from "@svgr/rollup";
-import image from "@rollup/plugin-image";
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
 import svg from "rollup-plugin-svg-import";
+import images from "rollup-plugin-image-files";
 
 const packageJson = require("./package.json");
 
@@ -17,6 +19,7 @@ export default [
         file: packageJson.main,
         format: "cjs",
         sourcemap: true,
+        name: "@lbui-lib",
       },
       {
         file: packageJson.module,
@@ -24,19 +27,24 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }), postcss()],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      images(),
+      svgr(),
+      svg({
+        stringify: false,
+      }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
+    ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [
-      dts(),
-      svgr(),
-      image(),
-      svg({
-        stringify: false,
-      }),
-    ],
+    plugins: [dts()],
     external: [/\.(css|less|scss)$/],
   },
 ];
